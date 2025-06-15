ed0e9e509390d22a3c5d2860f0a6f66ddf93508f
Imam Bahri Alwi <115518832+alwi2022@users.noreply.github.com>
2025-06-15 10:14:57 +0700

    fix/main.tsx import

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
+import "./index.css";
 import { LangProvider } from "./context/LangContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
