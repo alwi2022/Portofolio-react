0cfee77a46ca8b497dc42b22b775f19498d25ec5
Imam Bahri Alwi <115518832+alwi2022@users.noreply.github.com>
2025-06-15 10:12:12 +0700

    delete multilingual routing

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,12 @@ import SkillsSection from "./sections/SkillsSection";
 import ContactSection from "./sections/ContactSection";
 import { JSX } from "react";
 import Footer from "./sections/FooterSection";
-import { useEffect } from "react";
-import { useParams } from "react-router";
-import { useLang } from "./context/LangContext";
 
 function App(): JSX.Element {
-   const { lang: langParam } = useParams();
-  const { setLang } = useLang();
-
-  useEffect(() => {
-    if (langParam === "en" || langParam === "id") {
-      setLang(langParam);
-      document.documentElement.lang = langParam;
-    }
-  }, [langParam]);
   return (
     <div className="min-h-screen font-sans bg-white dark:bg-zinc-900 text-gray-900 dark:text-white transition-all">
-      {/* Header dengan navbar dan dark-mode toggle */}
       <Header />
 
-      {/* Main content */}
       <main className="px-4 sm:px-10 lg:px-20">
         <HeroSection />
         <ProjectsSection />
@@ -32,7 +18,6 @@ function App(): JSX.Element {
         <ContactSection />
       </main>
 
-      {/* Footer */}
       <Footer />
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
 // src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 import { LangProvider } from "./context/LangContext";
-import NotFoundPage from "./pages/NotFoundPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <LangProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Navigate to="/en" replace />} />
-          <Route path=":lang(en|id)" element={<App />} />
-          <Route path="*" element={<NotFoundPage />} />
+          <Route path="/" element={<App />} />
         </Routes>
       </BrowserRouter>
     </LangProvider>
